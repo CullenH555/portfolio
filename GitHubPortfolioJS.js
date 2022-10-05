62c4047abc2a42c5f21f5fe3f2a1d5532ce374ab
CullenH555 <68925720+CullenH555@users.noreply.github.com>
2022-10-05 13:47:08 -0700

    Add files via upload
    Added JS file for image display.

---
+++ GitHubPortfolioJS.js
@@ -0,0 +1,47 @@
+
+
+var order1 = ['builderPage', 'playmatPage', 'userPage', 'errorsPage'];
+var order2 = ['welcomePage', 'explorePage', 'preferencesPage', 'settingsPage', 'historyPage'];
+
+function nextEvent(direction, projectNumber) {
+    if (projectNumber == 1) {
+        var order = order1;
+        var hideElement = document.getElementsByClassName('show')[0];
+    }
+    else if (projectNumber == 2) {
+        var order = order2;
+        var hideElement = document.getElementsByClassName('show')[1];
+    }
+    var elementId = hideElement.getAttribute('id');
+    hideElement.removeAttribute('class');
+    hideElement.hidden = true;
+    for (var i=0; i<order.length; i++) {
+        if (order[i] == elementId) {
+            if (direction == 'previous') {
+                if (i == 0) {
+                    var showElement = document.getElementById(order[order.length-1]);
+                    showElement.setAttribute('class', 'show');
+                    showElement.hidden = false;
+                }
+                else {
+                    var showElement = document.getElementById(order[i-1]);
+                    showElement.setAttribute('class', 'show');
+                    showElement.hidden = false;
+                }
+            }
+            else if (direction == 'next') {
+                if (i == order.length-1) {
+                    var showElement = document.getElementById(order[0]);
+                    showElement.setAttribute('class', 'show');
+                    showElement.hidden = false;
+                }
+                else {
+                    var showElement = document.getElementById(order[i+1]);
+                    showElement.setAttribute('class', 'show');
+                    showElement.hidden = false;
+                }
+            }
+        }
+    }
+}
+
